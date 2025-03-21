11a668e3dcbe429bfd284ace9831e9230bf4b091
Bhashini123868 <bhashimunasinghe286@gmail.com>
2025-03-21 20:17:28 +0530

    first commit

--- src/app/page/dashboard/managecustomer/managecustomer.component.ts
+++ src/app/page/dashboard/managecustomer/managecustomer.component.ts
@@ -1,24 +1,39 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import Customer from '../../model/Customer';
 import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-managecustomer',
-  imports: [RouterLink, RouterOutlet],
+  selector: 'app-manage-customer',
+  imports: [CommonModule],
   templateUrl: './managecustomer.component.html',
   styleUrl: './managecustomer.component.css'
 })
 export class ManagecustomerComponent {
-  public customerList:any=[];
+
+  customerList:Customer[] =[];
+
   constructor(private http:HttpClient){
+    // new Customer(1,"saman",7500.0);
+
+    // let customer:Customer ={
+    //   id:1,
+    //   name:"saman",
+    //   salary:7500.0
+    // }
+
+    // let customerList:Customer[] = [
+    //   new Customer(1,"saman",7500.0),
+    //   new Customer(1,"saman",7500.0),
+    //   new Customer(1,"saman",7500.0),
+    // ]
+
     this.loadCustomer();
   }
   loadCustomer(){
-    this.http.get("http://localhost:8081/customer/get-all").subscribe(data=>{
-      console.log(data);
-      this.customerList=data;
-      
+    this.http.get<Customer[]>("http://localhost:8081/customer/get-all").subscribe(res=>{
+      this.customerList=res;
+      console.log(res);
     })
   }
 }
